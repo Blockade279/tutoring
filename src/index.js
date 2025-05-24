b55970efb113682f490b249ebbda28f4ca2a0d6e
Blockade279 <bbird1@highpoint.edu>
2025-05-24 12:58:04 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './App';
+
+ReactDOM.render(
+  <React.StrictMode><Home /></React.StrictMode>,
+  document.getElementById('root')
+);
 
